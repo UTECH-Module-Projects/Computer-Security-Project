03294439dd64a78abc96bb70783e0c4ce3742580
Rushawn White <rushawnwhite29@gmail.com>
2023-04-10 23:27:29 -0500

    Final Honeypot Implementation with GUI

--- honeypot.py
+++ honeypot.py
@@ -1,3 +1,5 @@
+import tkinter
+from tkinter import ttk
 from scapy.all import *
 from datetime import datetime
 
@@ -7,22 +9,51 @@ from scapy.layers.inet import TCP, IP
 PORTS = [22, 80, 443]
 
 
-# Logging function to log received data
-def log_data(port, data):
-    print(f"Data received on port {port}: {data.decode()}")
-    with open(f"honeypot_{port}_log.txt", "a") as f:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        f.write(f"[{timestamp}] Data received on port {port}: {data.decode()}\n")
-
-
-# Intrusion detection function
-def intrusion_detection(pack):
-    # Customize intrusion detection rules as needed
-    if pack[TCP].flags == 'S' and pack[TCP].dport in PORTS:
-        print(f"Intrusion detected! SYN scan on port {pack[TCP].dport} from {pack[IP].src}")
-        with open("intrusion_alerts.txt", "a") as f:
+class PortListener:
+    # Logging function to log received data
+    @staticmethod
+    def log_data(host, addr, port, data, tree):
+        print(f"Data received on port {port}: {data.decode()}")
+        with open(f"honeypot_{port}_log.txt", "a") as f:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            f.write(f"[{timestamp}] Intrusion detected! SYN scan on port {pack[TCP].dport} from {pack[IP].src}\n")
+            f.write(f"[{timestamp}] Data received on port {port}: {data.decode()}\n")
+            tree.insert("", "end", values=[timestamp, port, addr, host, data.decode()])
+
+    # Intrusion detection function
+    @staticmethod
+    def intrusion_detection(pack, tree):
+        # Customize intrusion detection rules as needed
+        if pack[TCP].flags == 'S' and pack[TCP].dport in PORTS:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{timestamp}] Intrusion detected! SYN scan. details: {{port={pack[TCP].dport}, srcIP={pack[IP].src}, dstIP={pack[IP].dst}}}")
+            with open("intrusion_alerts.txt", "a") as f:
+                f.write(f"[{timestamp}] Intrusion detected! SYN scan. details: {{port={pack[TCP].dport}, srcIP={pack[IP].src}, dstIP={pack[IP].dst}}}\n")
+                tree.insert("", "end", values=[timestamp, pack[TCP].dport, pack[IP].src, pack[IP].dst, "SYN scan"])
+
+        if pack.haslayer(Raw):
+            payload = pack[Raw].load.decode(errors='ignore')
+            if 'cmd.exe' in payload or 'root' in payload or 'sh' in payload:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"[{timestamp}] Intrusion detected! Suspicious payload. details: {{port={pack[TCP].dport}, srcIP={pack[IP].src}, dstIP={pack[IP].dst}}}")
+                with open("intrusion_alerts.txt", "a") as f:
+                    f.write(f"[{timestamp}] Intrusion detected! Suspicious payload. details: {{port={pack[TCP].dport}, srcIP={pack[IP].src}, dstIP={pack[IP].dst}}}\n")
+                    tree.insert("", "end", values=[timestamp, pack[TCP].dport, pack[IP].src, pack[IP].dst + "", "Suspicious payload"])
+
+
+class SniffThread(Thread):
+    def __init__(self, tree):
+        super().__init__()
+        self.tree = tree
+
+    def run(self) -> None:
+        try:
+            # Start packet capture
+            sniff(filter=f"tcp and (dst port {' or '.join(map(str, PORTS))})",
+                  prn=lambda pack: PortListener.intrusion_detection(pack, self.tree))
+
+        except KeyboardInterrupt:
+            print("Closing Sniff...")
+            return
 
 
 # Honeypot class
@@ -32,15 +63,35 @@ class Honeypot:
         self.max_connections = max_connections
 
     def start(self):
-        for port in PORTS:
-            # Create a new thread for each port
-            t = threading.Thread(target=self.listen, args=(port,))
-            t.start()
+        try:
+            root = tkinter.Tk()
+            root.title("Honeypot")
+            root.geometry("1000x400")
 
-        # Start packet capture
-        sniff(filter=f"tcp and (dst port {' or '.join(map(str, PORTS))})", prn=intrusion_detection)
+            # Create trace table
+            tree = ttk.Treeview(root, columns=("Timestamp", "Port", "Source IP", "Destination Port", "Result"), show="headings")
+            tree.heading("Timestamp", text="Timestamp")
+            tree.heading("Port", text="Port")
+            tree.heading("Source IP", text="Source IP")
+            tree.heading("Destination Port", text="Destination IP")
+            tree.heading("Result", text="Result")
+            tree.pack(expand=True, fill="both")
 
-    def listen(self, port):
+            for port in PORTS:
+                # Create a new thread for each port
+                t = threading.Thread(target=self.listen, args=(port, tree))
+                t.start()
+                time.sleep(0.1)
+
+            SniffThread(tree).start()
+
+            root.mainloop()
+
+        except KeyboardInterrupt:
+            print("Closing main app...")
+            return
+
+    def listen(self, port, tree):
         # Create a socket and bind it to the specified port
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((self.host, port))
@@ -57,7 +108,11 @@ class Honeypot:
                 data = conn.recv(1024)
                 if data:
                     # Log received data
-                    log_data(port, data)
+                    PortListener.log_data(conn.getsockname(), addr[0], port, data, tree)
+
+                    # Send a response to the client
+                    response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Welcome Hacker!</h1></body></html>".encode()
+                    conn.sendall(response)
 
                 # Close the connection
                 conn.close()
@@ -69,5 +124,4 @@ class Honeypot:
 
 
 if __name__ == '__main__':
-    honeypot = Honeypot()
-    honeypot.start()
+    Honeypot().start()
